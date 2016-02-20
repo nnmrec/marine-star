4444d8fadbdc2f1853f7016fb1789e0e1a9aa2f3
Danny Clay Sale <dsale@uw.edu>
2016-02-19 16:27:43 -0800

    update format of mooring code input/output

--- Mezzanine/utilities/mooring/mooring.m
+++ Mezzanine/utilities/mooring/mooring.m
@@ -1,66 +1,48 @@
 function [probes, rotors] = mooring(OPTIONS, probes, rotors)
 
 
-%% reshape the data into column vectors, the format that mooring code prefers
-% coordinates at points along the mooring lines, and the buoyancy pods (plus anything else not a turbine)
+%% reshape the data into format that mooring code prefers
+% probes: coordinates at points along the mooring lines, and the buoyancy pods (plus anything else not a turbine)
 xyz_probes = probes.xyz;
 vel_probes = probes.vel;
 
-% coordinates of turbines (center of rotor)
+% rotors: coordinates of turbines (center of rotor)
 xyz_rotors = rotors.data(:,2:4);
 ang_rotors = rotors.data(:,5:7);
 vel_rotors = rotors.vel;
 thr_rotors = rotors.thrust;
 tor_rotors = rotors.torque;
 
-% positions: body nodes and then append line nodes: [x y z nx ny nz, x y z nx ny nz, ..., x y z, x y z, ...]
-q_body = [];
-q_line = [];
-
-% velocities: body nodes and then append line nodes: [Ux Uy Uz, Ux Uy Uz, ..., Ux Uy Uz, Ux Uy Uz, ...]
-v_body = [];
-v_line = [];
-
-% forces and moments: body nodes: [Fx Fy Fz Mx My Mz, Fx Fy Fz Mx My Mz, ... , Fx Fy Fz, Fx Fy Fz, ...]
-f_body = [];
-f_line = [];
-
-% append (not as efficient because growing inside the loop, but easy)
+% BODY nodes
 nBodies = size(xyz_rotors,1);
+q_body  = zeros(nBodies, 6);
+v_body  = zeros(nBodies, 1);
+f_body  = zeros(nBodies, 6);
 for n = 1:nBodies
-    q_body = [q_body; xyz_rotors(n,1)]; % x
-    q_body = [q_body; xyz_rotors(n,2)]; % y
-    q_body = [q_body; xyz_rotors(n,3)]; % z
-    
-    v_body = [v_body; vel_rotors(n)];   % "reference" inflow velocity
-    
-    f_body = [f_body; thr_rotors(n)];   % Fx
-    f_body = [f_body; 0];               % Fy
-    f_body = [f_body; 0];               % Fz
-    f_body = [f_body; 0];               % Mx
-    f_body = [f_body; 0];               % My
-    f_body = [f_body; tor_rotors];      % Mz
-end
-nLines = size(xyz_probes,1);
-for n = 1:nLines
-    q_line = [q_line; xyz_probes(n,1)]; % x
-    q_line = [q_line; xyz_probes(n,2)]; % y
-    q_line = [q_line; xyz_probes(n,3)]; % z
-    
-    v_line = [v_line; vel_probes(n)];   % "reference" inflow velocity
-    
-    f_line = [f_line; 0];               % Fx (Tyler: you will need to add your drag force calculations here, I imagine some angles will give X, Y, and Z force components)
-    f_line = [f_line; 0];               % Fy
-    f_line = [f_line; 0];               % Fz
+    % positions (x,y,z)
+    q_body(n,:) = [xyz_rotors(n,:), ang_rotors(n,:)];    
+    % inflow velocities (velocity magnitude, volume avg. upsteam of rotor)
+    v_body(n)   = vel_rotors(n);
+    % forces (Fx=thrust,Fy,Fz,Mx,My,Mz=torque)
+    f_body(n,:) = [thr_rotors(n), 0, 0, 0, 0, tor_rotors(n)];
 end
 
-% append bodies and lines
-q = [q_body; q_line];
-v = [v_body; v_line];
-f = [f_body; f_line];
+% LINE nodes
+nLines = size(xyz_probes,1);
+q_line = zeros(nLines, 3);
+v_line = zeros(nLines, 1);
+f_line = zeros(nLines, 3);
+for n = 1:nLines
+    % positions (x,y,z)
+    q_line(n,:) = xyz_probes(n,:);    
+    % inflow velocities (velocity magnitude, volume avg. upsteam of rotor)
+    v_line(n)   = vel_probes(n);
+    % forces (Fx,Fy,Fz) ... (you will need to add your drag force calculations here, I imagine some angles will give X, Y, and Z force components, but assume 0 for now)
+    f_line(n,:) = [0,0,0];
+end
 
 %% run the equilibrium mooring model to calculate new positions
-% new_xyz = mooring_equilibrium_solver(OPTIONS, q, v, f);
+% new_xyz = mooring_equilibrium_solver(OPTIONS,q_body,v_body,f_body,q_line,v_line,f_line);
 
 % for now just move the coordinates randomly, to demostrate the code "works"
 % new_xyz.probes = xyz_probes;
